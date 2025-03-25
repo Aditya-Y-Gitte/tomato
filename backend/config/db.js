a3ac7adebb4b9a3522ef707f45f94b64e5ab3fc2
Aditya Gitte <BE21F05F028@geca.ac.in>
2025-03-26 00:07:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async ()=>{
-    await mongoose.connect('mongodb+srv://ybangar20:yogesh2042003@cluster0.fn3mfcw.mongodb.net/food-del').then(()=>console.log("db connected"));
+    await mongoose.connect('').then(()=>console.log("db connected"));
 }
